c41bdaba11a5f4ea47336f8f09aed62f184f83d9
Jay Wongkot <onzonstudios@gmail.com>
2020-11-19 21:29:39 -0400

    Setting up Authentication

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Header from './Header';
+import Login from './Login';
 
 function App() {
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className='App'>
       <Router>
         <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
           <Route path='/'>
             <Header />
             <Home />
